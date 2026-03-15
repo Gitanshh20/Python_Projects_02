c73dfcd07fb6f5515dc2938446bacd3c52dc1609
GitanshDev <gitanshagalcha@gmail.com>
2026-03-15 10:51:20 +0530

    Python/NumberGuessing.py

--- Python/NumberGuessing.py
+++ Python/NumberGuessing.py
@@ -8,22 +8,28 @@ num = random.randint(1, 100)
 print("Welcome to Number Guessing Python Program\n")
 
 Ask = input("If You Want to Start Type 'Yes' or 'No' to denied:- ")
+
+if Ask == "No":
+  print("Ok, Bye We can Play Next Time")
   
-def Guess_Loop():
-    
-    while True:
 
-        if Ask == "Yes":
-            Guess_num = int(input("Enter Your Guess: "))
+if Ask == "Yes":
+    def Guess_Loop():
+        
+        while True:
+            
+            if Ask == "Yes":
+                Guess_num = int(input("Enter Your Guess: "))
+                
+            if Guess_num > num :
+                print("\nToo High!, Try Again.")
+                
+            elif num > Guess_num :
+                print("\nToo Low!, Try Again.")
+                
+            else:
+                print("\nCongress, You are right this is your Number..")
+                break
 
-        if Guess_num > num :
-            print("Too High!, Try Again.")
 
-        elif num > Guess_num :
-            print("Too Low!, Try Again.")
-
-        else:
-            print("Congress, You are right this is your Number..")
-            break
-    
-Guess_Loop()
+    Guess_Loop()
